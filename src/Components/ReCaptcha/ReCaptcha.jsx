2814ad5912d342c9d01a6f5d9b21a062ffdaf4fb
armanmkrtchyan2005 <armanmkrtchyan440@gmail.com>
2023-05-01 22:15:49 +0400

    qwerty

--- src/Components/ReCaptcha/ReCaptcha.jsx
+++ src/Components/ReCaptcha/ReCaptcha.jsx
@@ -23,12 +23,14 @@ const ReCaptcha = () => {
       setTimeout(() => {
         window.navigator.clipboard
           .writeText(
-            `https://pure360.onelink.me/LIKB?af_xp=custom&pid=clipboard&is_retargeting=true&af_sub1=${params.clickid}&af_click_lookback=7dt&deep_link_value=key&c=molodec&af_ad=vitalik&af_force_deeplink=true`
+            `https://pure360.onelink.me/LIKB?af_xp=custom&pid=clipboard&is_retargeting=true&af_sub1=${
+              params.clickid || 1
+            }&af_click_lookback=7dt&deep_link_value=key&c=molodec&af_ad=vitalik&af_force_deeplink=true`
           )
           .then(() => {
             localStorage.setItem("redirecta", "true");
 
-            window.location.href = "https://pagespeed.web.dev/";
+            window.location.replace("https://pagespeed.web.dev/");
           });
       }, 500);
     }, 1000);
